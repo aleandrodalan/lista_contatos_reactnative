c65f79380b9a6bd688c9621aefdf3445de3bafa1
Aleandro P. Dalan <aleandro.dalan@loglabdigital.com.br>
2020-09-20 21:15:55 -0400

    Terceiro commit

--- App.js
+++ App.js
@@ -2,22 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, NativeModules } from 'react-native';
 
+import axios from 'axios';
+
 import Header from './src/components/Header/Header';
 
 export default class App extends React.Component {
   renderList() {
-    const nomes = [
-      'Eddie Van Halen',
-      'Jimi Hendrix',
-      'Chimbinha',
-      'Steve Vai'
-    ];
+//    const textElements = nomes.map((name) => {
+//      return <Text key={name}>{name}</Text>
+//    });
 
-    const textElements = nomes.map((name) => {
-      return <Text key={name}>{name}</Text>
-    });
-
-    return textElements;
+    axios
+      .get('https://randomuser.me/api/?nat=br&results=5')
+      .then(response => { 
+        const { results } = response.data
+        const { names } = results.map(people => people.name.first)
+        console.log(names);
+      });
   }
   
   render() {
